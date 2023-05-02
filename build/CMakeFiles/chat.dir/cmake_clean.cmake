1affc914d7cc744e4ada1155ac79614a37155430
volrus77 <vgruzdev@yandex.ru>
2023-05-02 21:55:12 +0300

    старт

---
+++ build/CMakeFiles/chat.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat.dir/src/chat.cpp.o"
+  "CMakeFiles/chat.dir/src/chat.cpp.o.d"
+  "CMakeFiles/chat.dir/src/main.cpp.o"
+  "CMakeFiles/chat.dir/src/main.cpp.o.d"
+  "CMakeFiles/chat.dir/src/sha1.cpp.o"
+  "CMakeFiles/chat.dir/src/sha1.cpp.o.d"
+  "chat"
+  "chat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
